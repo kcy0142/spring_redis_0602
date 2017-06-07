7399c1e024c2eea1343819e34e66fe1b72490a95
jameskim <kcy0142@naver.com>
2017-06-07 17:10:41 +0900

    Update RedisMessageListenerIntegrationTest.java

--- src/test/java/com/example/demo/RedisMessageListenerIntegrationTest.java
+++ src/test/java/com/example/demo/RedisMessageListenerIntegrationTest.java
@@ -1,9 +1,30 @@
 package com.example.demo;
 
-import com.example.config.RedisConfig;import com.example.queue.MessagePublisher;import com.example.queue.RedisMessageSubscriber;import org.junit.Test;import org.junit.runner.RunWith;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.test.context.ContextConfiguration;import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.example.config.RedisConfig;
+import com.example.queue.MessagePublisher;
+import com.example.queue.RedisMessageSubscriber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.UUID;
 import static org.junit.Assert.assertTrue;
-@RunWith(SpringJUnit4ClassRunner.class)@ContextConfiguration(classes = RedisConfig.class)public class RedisMessageListenerIntegrationTest {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = RedisConfig.class)
+public class RedisMessageListenerIntegrationTest {
  private MessagePublisher redisMessagePublisher;
- @Autowired public void setMessagePublisher(MessagePublisher redisMessagePublisher) {  this.redisMessagePublisher = redisMessagePublisher; }  //  @Autowired  //  private MessagePublisherImpl redisMessagePublisher;
-    @Test    public void testOnMessage() throws Exception {        String message = "Message test" + UUID.randomUUID();                redisMessagePublisher.publish(message);        redisMessagePublisher.publishTopic("test",message);        Thread.sleep(100);              assertTrue(RedisMessageSubscriber.messageList.get(0).contains(message));    }}
+ @Autowired 
+  public void setMessagePublisher(MessagePublisher redisMessagePublisher) { 
+    this.redisMessagePublisher = redisMessagePublisher;
+  } 
+  //  @Autowired  //  private MessagePublisherImpl redisMessagePublisher;
+    @Test  
+  public void testOnMessage() throws Exception {   
+    String message = "Message test" + UUID.randomUUID();    
+    redisMessagePublisher.publish(message);     
+    redisMessagePublisher.publishTopic("test",message); 
+    Thread.sleep(100);          
+    assertTrue(RedisMessageSubscriber.messageList.get(0).contains(message));
+  }}
